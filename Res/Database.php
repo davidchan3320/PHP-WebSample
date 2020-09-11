555fecf9b454ddfdabea064df1f2f136121322f1
davidchan3320 <70759090+davidchan3320@users.noreply.github.com>
2020-09-12 05:49:31 +0800

    Update Database.php

--- Res/Database.php
+++ Res/Database.php
@@ -18,29 +18,6 @@ class dbManager
         return $this->db;
     }
 
-    // public function setup()
-    // {
-    //     if ($this->db !== false) {
-    //         $clinics = ($this->db)->ATWD->Clinics;
-    //         $data = json_decode(file_get_contents("http://www.ha.org.hk/opendata/facility-gop.json"));
-    //         if (isset($data)) {
-    //             $clinics->deleteMany([]);
-    //             $result = $clinics->insertMany($data);
-    //         }
-    //         // $resultMsg = printf("Inserted %d document(s)<br>", $result->getInsertedCount());
-    //         // $resultMsg .= var_dump($result->getInsertedIds());
-    //         // return $resultMsg;
-    //         // $error = new errorManager();
-    //         // $error->setErrorCodes(201);
-    //         // echo json_encode($error->getErrorIntoArray());
-    //     } else {
-    //         // $error = new errorManager();
-    //         // $error->setErrorCodes(401);
-    //         // echo json_encode($error->getErrorIntoArray());
-    //         // return "Get Clinics Data Failed!";
-    //     }
-    // }
-
     function toRegex($str)
     {
         return new MongoDB\BSON\Regex($str, "i");
